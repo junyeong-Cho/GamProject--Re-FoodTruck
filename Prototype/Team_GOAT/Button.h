9c6f8f0b715c2cab77649f8d5d5372acd8894ede
junhyeongKim-digipen <junhyeong.k@digipen.edu>
2023-03-28 16:57:53 +0900

    2023_03_28 Prototype
    main.cpp
    Button.h,Button.cpp(Struct Button)
    State.h(enum class State)

---
+++ Prototype/Team_GOAT/Button.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <string>
+#include <doodle/doodle.hpp>
+#include "State.h"
+
+struct Button
+{
+    //Button's position
+    double      x;
+    double      y;
+
+    //Button's Size
+    double      width;
+    double      height;
+
+    //State
+    State* current_state;
+
+    //Button_name
+    std::string   buttonName;
+
+    //Check_button
+    bool checkMouse = false;
+
+    /** x좌표, y좌표, 버튼 가로길이, 버튼 세로길이, 현재장면변수 순으로 적으세용*/
+    Button(double tempX, double tempY, double tempWidth, double tempHeight, State& current_state)
+        : x(tempX), y(tempY), width(tempWidth), height(tempHeight), current_state(&current_state)
+    {
+    };
+
+    //have name button
+    //Button(double tempX, double tempY, double tempWidth, double tempHeight, std::string name)
+    //    : x(tempX), y(tempY), width(tempWidth), height(tempHeight), buttonName(name) {};
+
+
+    void draw(const int mouse_x, const int mouse_y);
+    void check_click(const int mouse_x, const int mouse_y, State next_state);
+
+    /** 마우스x좌표, 마우스y좌표, 다음장면 순으로 적으세용*/
+    void update(const int mouse_x, const int mouse_y, State next_state);
+};
---
+++ Prototype/Team_GOAT/State.h
@@ -0,0 +1,9 @@
+#pragma once
+
+enum class State
+{
+    Main,
+    Counter,
+    Kitchen
+};
+
