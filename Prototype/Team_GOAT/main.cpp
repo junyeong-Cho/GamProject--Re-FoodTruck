a7fbc27d9f987e465c437bdef0463f1be7d08ae8
junhyeongKim-digipen <junhyeong.k@digipen.edu>
2023-04-02 21:45:33 +0900

    design_the reaction of a guest

--- Prototype/Team_GOAT/main.cpp
+++ Prototype/Team_GOAT/main.cpp
@@ -8,7 +8,7 @@ void jh_Updatd()
     State state = State::Main;
 
     Button main_button(-100, -100, 200, 300, state);
-    Button counter_button(-100, -100, 200, 300, state);
+    Button counter_button((doodle::Width /14) * 5.5, (doodle::Height / 8.0) * 3.0, doodle::Width / 28.0, doodle::Height / 16.0, state);
     Button game_over_button(100, 100, 100, 100, state);
 
 
@@ -22,17 +22,29 @@ void jh_Updatd()
         case State::Main:
             doodle::clear_background(255, 255, 255);
             main_button.update(doodle::get_mouse_x(), doodle::get_mouse_y(), State::Counter);
-
+            
             break;
         case State::Counter:
-            doodle::clear_background(0, 255, 255);
-            doodle::draw_rectangle(-(doodle::Width/2.0) , 300, doodle::Width / 2.0, doodle::Height / 2.0);
+            doodle::clear_background(255, 255, 255);
+            doodle::push_settings();
+            
+            //speech bubble
+            doodle::set_fill_color(0,88);
+            doodle::draw_rectangle(-((doodle::Width / 7.0) * 3.0), -((doodle::Height / 8) * 3), ((doodle::Width / 7.0) * 6.0), ((doodle::Height / 8.0) * 5.0));
+            
+            //customer's satisfaction - 추후 추가 예정
+            doodle::draw_rectangle(-((doodle::Width / 7.0) * 3.0), ((doodle::Height / 8.0) * 2.0), (doodle::Width / 7.0) * 2.0, (doodle::Height / 8.0) );
+            doodle::draw_text(" 0 % / o x o", -((doodle::Width / 7.0) * 3.0), ((doodle::Height / 8.0) * 2.0));
+            doodle::pop_settings();
+
             counter_button.update(doodle::get_mouse_x(), doodle::get_mouse_y(), State::Game_over);
-             
             break;
         case State::Game_over:
             doodle::clear_background(255, 255, 255);
-            
+            doodle::push_settings();
+            doodle::set_font_size(100.0);
+            doodle::draw_text(" RE \n Try?", -((doodle::Width / 7.0) * 3.0), -((doodle::Height / 8.0) * 0.5));
+            doodle::pop_settings();
             game_over_button.update(doodle::get_mouse_x(), doodle::get_mouse_y(), State::Counter);
             break;
 
@@ -42,7 +54,7 @@ void jh_Updatd()
 
 int main(void)
 {
-    doodle::create_window("Team_GOAT");
+    doodle::create_window("Team_GOAT", 1400, 800);
     jh_Updatd();
 
 }
