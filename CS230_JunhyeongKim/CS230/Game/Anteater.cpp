16bb63f0d4a0027ee3bb58b4b5abb3b288505973
Junyeong-Cho <tpqldj1312@gmail.com>
2023-05-28 16:32:23 +0900

    ...

---
+++ CS230_JunhyeongKim/CS230/Game/Anteater.cpp
@@ -0,0 +1,17 @@
+/*
+Copyright (C) 2023 DigiPen Institute of Technology
+Reproduction or distribution of this file or its contents without
+prior written consent is prohibited
+File Name:  Anteater.cpp
+Project:    CS230 Engine
+Author:     Junyeong Cho
+Created:    May 24, 2023
+*/
+
+#include "Anteater.h"
+
+Anteater::Anteater(Customor* front)
+	:Customor(front)
+{
+	sprite.Load("Assets/Anteater.spt");
+}
